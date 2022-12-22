e5ec50c3a175319d0cc22d08bde79292b67a9013
foesublime <rylandcs@gmail.com>
2022-12-22 12:22:34 -0700

    added, js index for DL

---
+++ index.js
@@ -0,0 +1,23 @@
+// Drivers license String
+
+var surname = 'Dingus';
+var dateOfBirth = '04/04/1990';
+var age = 32;
+var isOrganDonor = 'false';
+var streetAddress = '1234 asdfg street';
+var city = 'Denver';
+var state = 'Colorado';
+var zip = 80027;
+var idNumber = 382734758658;
+var height = 7;
+
+
+// String Concatenation 
+var myId = 'My name is' + surname + 'I was born on' + 'dateOfBirth' +
+'I am' + 'age' + 'my organ donor status is' + 'I live at' + 'streetAddress'
++ 'in the city of' + 'city' + 'in the state of' + 'state' + 'in the area of'
++ 'zip' + 'my license id number is' + 'idNumber' + 'my heigh is' + 'height';
+
+console.log(myId);
+console.log(typeof myId);
+console.log(myId.length)
